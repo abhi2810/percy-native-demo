a6576f761f57b276f0780e165a5d2cdaca93ef17
Abhi Singh <abhi.s@browserstack.com>
2021-05-26 10:09:20 +0530

    first commit

---
+++ create-demo-pr.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+set -e
+
+if [ "$BROWSERSTACK_USERNAME" == "" ] || [ "$BROWSERSTACK_ACCESS_KEY" == "" ]; then
+  echo 'Error: Please initialize environment variables BROWSERSTACK_USERNAME and BROWSERSTACK_ACCESS_KEY with your BrowserStack account username and access key, before running tests'
+  exit 1
+fi
+
+# Check if app is uploaded if not then upload new app
+var="$(curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" -X GET "https://api-cloud.browserstack.com/app-automate/recent_apps/PercyAndroid" 2>/dev/null)"
+echo "$var"
+if [[ "$var" =~ ^{.*}$ ]] || [ "$var" == "" ]; then
+  echo "Uploading the App"
+  curl -u "$BROWSERSTACK_USERNAME:$BROWSERSTACK_ACCESS_KEY" \
+  -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
+  -F "file=@./app/app.apk" \
+  -F "custom_id=PercyAndroid"
+fi
+
+# if ! [ -x "$(command -v hub)" ]; then
+#   echo 'Error: hub is not installed (https://hub.github.com/). Please run "brew install hub".' >&2
+#   exit 1
+# fi
+
+
+NOW=`date +%d%H%M%S`
+BASE_BRANCH="master-$NOW"
+BRANCH="update-button-$NOW"
+if [ "$CI_USER_ID" != "" ];
+then
+  BASE_BRANCH=${CI_USER_ID}_${BASE_BRANCH}
+  BRANCH=${CI_USER_ID}_${BRANCH}
+fi
+
+# cd to current directory as root of script
+cd "$(dirname "$0")"
+
+mkdir -p images
+
+# Create a "master-123123" branch for the PR's baseline.
+# This allows demo PRs to be merged without fear of breaking the actual master.
+# git checkout master
+# git checkout -b $BASE_BRANCH
+# git push origin $BASE_BRANCH
+
+# Create the update-button-123123 PR. It is always a fork of the update-button-base branch.
+# git checkout update-button-base
+# git checkout -b $BRANCH
+# git commit --amend -m 'Change Sign Up button style.'
+# git push origin $BRANCH
+# PR_NUM=$(hub pull-request -b $BASE_BRANCH -m 'Change Sign Up button style.' | grep -oE '[0-9]+')
+
+export PERCY_BRANCH=$BRANCH
+# export PERCY_PULL_REQUEST=$PR_NUM
+
+npx http-server -p 5162 &
+
+npm test
+# npx percy upload ./images
+
+# Create the fake "ci/service: Tests passed" notification on the PR.
+# Uses a personal access token (https://github.com/settings/tokens) which has scope "repo:status".
+# curl \
+#   -u $GITHUB_USER:$GITHUB_TOKEN \
+#   -d '{"state": "success", "target_url": "https://example.com/build/status", "description": "Tests passed", "context": "ci/service"}' \
+#   "https://api.github.com/repos/BrowserStackCE/percy-demo/statuses/$(git rev-parse --verify HEAD)"
+
+# git checkout master
+# git branch -D $BASE_BRANCH
+# git branch -D $BRANCH
+
+jobs
+kill %1
+wait
---
+++ test.js
@@ -0,0 +1,54 @@
+const wdio = require("webdriverio");
+const assert = require("assert");
+const browserstack = require("browserstack-local");
+
+const opts = {
+    hostname: 'hub-cloud.browserstack.com',
+    path: '/wd/hub',
+    capabilities: {
+        "os_version": "8.0",
+        "device": "Samsung Galaxy S9",
+        "app": "PercyAndroid",
+        "build": "Percy Build",
+        "name": "Percy Session",
+        "browserstack.local": true,
+        "browserstack.user": process.env.BROWSERSTACK_USERNAME,
+        "browserstack.key": process.env.BROWSERSTACK_ACCESS_KEY
+    }
+};
+
+var bs_local = new browserstack.Local();
+
+async function main() {
+    var bs_local_args = { 'key': process.env.BROWSERSTACK_ACCESS_KEY };
+    await new Promise((resolve, reject) => {
+        bs_local.start(bs_local_args, function () {
+            console.log("Started BrowserStackLocal");
+            resolve();
+        });
+    });
+    const browser = await wdio.remote(opts);
+    await browser.pause(5000);
+    var context = await browser.getContexts();
+    console.log(context);
+    await browser.switchContext(context[1]);
+    console.log(await browser.getUrl());
+    await browser.saveScreenshot('./images/dashboard.png');
+    await browser.url('http://bs-local.com:5162/assets/login.html');
+    await browser.saveScreenshot('./images/login.png');
+    await browser.url('http://bs-local.com:5162/assets/signup.html');
+    await browser.saveScreenshot('./images/signup.png');
+    await browser.url('http://bs-local.com:5162/assets/404_error.html');
+    await browser.saveScreenshot('./images/404_error.png');
+    await browser.url('http://bs-local.com:5162/assets/500_error.html');
+    await browser.saveScreenshot('./images/500_error.png');
+    await browser.deleteSession();
+    await new Promise((resolve, reject) => {
+        bs_local.stop(function () {
+            console.log("Stopped BrowserStackLocal");
+            resolve();
+        });
+    });
+}
+
+main();
