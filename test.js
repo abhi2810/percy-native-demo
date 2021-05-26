1a1af5776f2c93d27eaf0033f47ba85ed9964a33
Abhi Singh <abhi.s@browserstack.com>
2021-05-26 14:59:06 +0530

    added screenshot bug fix

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const wdio = require("webdriverio");
-const assert = require("assert");
 const browserstack = require("browserstack-local");
+const Jimp = require("jimp");
 
 const opts = {
     hostname: 'hub-cloud.browserstack.com',
@@ -19,6 +19,21 @@ const opts = {
 
 var bs_local = new browserstack.Local();
 
+async function getFrameLocationSize(browser) {
+    var frame = await browser.$('(//android.widget.FrameLayout)[2]');
+    var location = await frame.getLocation();
+    var size = await frame.getSize();
+    return [location.x, location.y, size.width, size.height];
+}
+
+async function takeScreenshot(browser, uri, frameLocationSize) {
+    await browser.saveScreenshot(uri);
+    Jimp.read(uri)
+        .then(image => {
+            return image.crop(...frameLocationSize).write(uri);
+        });
+}
+
 async function main() {
     var bs_local_args = { 'key': process.env.BROWSERSTACK_ACCESS_KEY };
     await new Promise((resolve, reject) => {
@@ -29,23 +44,24 @@ async function main() {
     });
     const browser = await wdio.remote(opts);
     await browser.pause(2000);
+    const frameLocationSize = await getFrameLocationSize(browser);
     var context = await browser.getContexts();
     await browser.switchContext(context[1]);
     await browser.url('http://bs-local.com:5162/assets/index.html');
     await browser.pause(2000);
-    await browser.saveScreenshot('./images/dashboard.png');
+    await takeScreenshot(browser, './images/dashboard.png', frameLocationSize);
     await browser.url('http://bs-local.com:5162/assets/login.html');
     await browser.pause(2000);
-    await browser.saveScreenshot('./images/login.png');
+    await takeScreenshot(browser, './images/login.png', frameLocationSize);
     await browser.url('http://bs-local.com:5162/assets/signup.html');
     await browser.pause(2000);
-    await browser.saveScreenshot('./images/signup.png');
+    await takeScreenshot(browser, './images/signup.png', frameLocationSize);
     await browser.url('http://bs-local.com:5162/assets/404_error.html');
     await browser.pause(2000);
-    await browser.saveScreenshot('./images/404_error.png');
+    await takeScreenshot(browser, './images/404_error.png', frameLocationSize);
     await browser.url('http://bs-local.com:5162/assets/500_error.html');
     await browser.pause(2000);
-    await browser.saveScreenshot('./images/500_error.png');
+    await takeScreenshot(browser, './images/500_error.png', frameLocationSize);
     await browser.deleteSession();
     await new Promise((resolve, reject) => {
         bs_local.stop(function () {
